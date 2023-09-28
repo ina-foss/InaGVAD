6caa5b6ac97185957ec5beb9f5d5b386ccede4b0
David Doukhan <david.doukhan@gmail.com>
2023-09-28 17:48:32 +0200

    vad script src

---
+++ inaGVAD/vad_metrics.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# The MIT License
+
+# Copyright (c) 2023 Ina (David Doukhan - http://www.ina.fr/)
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import glob
+import pandas as pd
+from os.path import splitext, basename
+from pyannote.core import Annotation, Timeline, Segment
+from pyannote.metrics.detection import DetectionAccuracy, DetectionPrecisionRecallFMeasure
+
+def csv2annot(fname):
+    """
+    Convert a csv with fields start, stop, label to pyannote annotation
+    """
+    df = pd.read_csv(fname)
+    an = Annotation()
+    for t in df.itertuples():
+        an[Segment(t.start, t.stop)] = t.label
+    return an
+
+def annot2vad(annot):
+    tl = Timeline()
+    for seg, track, label in annot.itertracks(yield_label=True):
+        if label in ['male', 'female', 'speech']:
+            tl.add(seg)
+    return tl.support().to_annotation()
+
+class VadEval:
+    def __init__(self, collar):
+        self.da = DetectionAccuracy(collar=collar)
+        self.dprf = DetectionPrecisionRecallFMeasure(collar=collar)
+    
+   
+    def __call__(self, ref, pred, uem, basename=''):
+        comp = self.da.compute_components(ref, pred, uem=uem)
+        comp.update(self.dprf.compute_components(ref, pred, uem=uem))
+        comp['basename'] = basename
+        comp['accuracy'] = self.da.compute_metric(comp)
+        prf = self.dprf.compute_metrics(comp)
+        for i, name in [(0, 'precision'), (1, 'recall'), (2, 'fmeasure')]:
+            comp[name] = prf[i]
+        return comp
+    
+    def compare_csv(self, ref_csv, pred_csv):
+        base, _ = splitext(basename(ref_csv))
+        refannot = csv2annot(ref_csv)
+        uem = refannot.get_timeline().support()
+        ref = annot2vad(refannot)
+        pred = annot2vad(csv2annot(pred_csv))
+        return self(ref, pred, uem, base)
+    
+    def compare_lfiles(self, lref, lpred):
+        dret = [self.compare_csv(ref, pred) for ref, pred in zip(lref, lpred)]
+        df = pd.DataFrame.from_dict(dret)
+        dfsum = df.sum()
+        ret = {'accuracy' : self.da.compute_metric(dfsum)}
+        ret['precision'], ret['recall'], ret['fmeasure'] = self.dprf.compute_metrics(dfsum)
+        return df, ret
+    
+    def compare_directories(self, ref_dir, pred_dir, verbose=False):
+        lref = glob.glob(ref_dir + '/*.csv')
+        lpred = ['%s/%s' % (pred_dir, basename(ref)) for ref in lref]
+        return self.compare_lfiles(lref, lpred)
+    
+    def compare_category(self, pred_dir, criterion, csvfname='./VAD_results_extended.csv'):
+
+        df = pd.read_csv(csvfname)
+        lret = []
+        ldf = []
+        
+        for k, sdf in df.groupby(criterion):
+            src = sdf.basename.map(lambda x: './annotations/vad/%s.csv' % x)
+            dst = sdf.basename.map(lambda x: '%s/%s.csv' % (pred_dir, x))
+            dfdetail, dret = self.compare_lfiles(src, dst)
+            d = {'category': k}
+            d.update(dret)
+            dfdetail['category'] = k
+            ldf.append(dfdetail)
+            lret.append(d)
+        return pd.concat(ldf), pd.DataFrame.from_dict(lret)
