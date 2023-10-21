32bbdef7b227588a279eaf213d93c5c6a0671275
David Doukhan <david.doukhan@gmail.com>
2023-10-21 13:52:05 +0200

    vad metrics

--- inaGVAD/vad_metrics.py
+++ inaGVAD/vad_metrics.py
@@ -34,7 +34,7 @@ def csv2annot(fname):
     Convert a csv with fields start, stop, label to pyannote annotation
     """
     df = pd.read_csv(fname)
-    an = Annotation()
+    an = Annotation(uri=basename(splitext(fname)[0]))
     for t in df.itertuples():
         an[Segment(t.start, t.stop)] = t.label
     return an
@@ -50,38 +50,48 @@ class VadEval:
     def __init__(self, collar):
         self.da = DetectionAccuracy(collar=collar)
         self.dprf = DetectionPrecisionRecallFMeasure(collar=collar)
-    
+
+    def reset(self):
+        self.da.reset()
+        self.dprf.reset()
    
-    def __call__(self, ref, pred, uem, basename=''):
+    def __call__(self, ref, pred, uem, fileid='', reset=True):
         comp = self.da.compute_components(ref, pred, uem=uem)
         comp.update(self.dprf.compute_components(ref, pred, uem=uem))
-        comp['basename'] = basename
+        comp['fileid'] = fileid
         comp['accuracy'] = self.da.compute_metric(comp)
         prf = self.dprf.compute_metrics(comp)
         for i, name in [(0, 'precision'), (1, 'recall'), (2, 'fmeasure')]:
             comp[name] = prf[i]
+        if reset:
+            self.reset()
         return comp
     
-    def compare_csv(self, ref_csv, pred_csv):
+    def compare_csv(self, ref_csv, pred_csv, reset=False):
         base, _ = splitext(basename(ref_csv))
         refannot = csv2annot(ref_csv)
         uem = refannot.get_timeline().support()
         ref = annot2vad(refannot)
         pred = annot2vad(csv2annot(pred_csv))
-        return self(ref, pred, uem, base)
+        return self(ref, pred, uem, base, reset=reset)
     
-    def compare_lfiles(self, lref, lpred):
-        dret = [self.compare_csv(ref, pred) for ref, pred in zip(lref, lpred)]
+    def compare_lfiles(self, lref, lpred, reset=True):
+        """
+        return detailed dataframe + global metrics
+        """
+        dret = [self.compare_csv(ref, pred, reset=False) for ref, pred in zip(lref, lpred)]
         df = pd.DataFrame.from_dict(dret)
         dfsum = df.sum()
         ret = {'accuracy' : self.da.compute_metric(dfsum)}
         ret['precision'], ret['recall'], ret['fmeasure'] = self.dprf.compute_metrics(dfsum)
+        if reset:
+            self.reset()
         return df, ret
     
-    def compare_directories(self, ref_dir, pred_dir, verbose=False):
-        lref = glob.glob(ref_dir + '/*.csv')
+    def compare_directories(self, ref_dir, pred_dir, verbose=False, reset=True):
+        lref = sorted(glob.glob(ref_dir + '/*.csv'))
         lpred = ['%s/%s' % (pred_dir, basename(ref)) for ref in lref]
-        return self.compare_lfiles(lref, lpred)
+        return self.compare_lfiles(lref, lpred, reset=reset)
     
     def compare_category(self, pred_dir, criterion, csvfname='./VAD_results_extended.csv'):
 
@@ -90,12 +100,22 @@ class VadEval:
         ldf = []
         
         for k, sdf in df.groupby(criterion):
-            src = sdf.basename.map(lambda x: './annotations/vad/%s.csv' % x)
-            dst = sdf.basename.map(lambda x: '%s/%s.csv' % (pred_dir, x))
+            src = sdf.fileid.map(lambda x: './annotations/vad/%s.csv' % x)
+            dst = sdf.fileid.map(lambda x: '%s/%s.csv' % (pred_dir, x))
             dfdetail, dret = self.compare_lfiles(src, dst)
             d = {'category': k}
             d.update(dret)
             dfdetail['category'] = k
             ldf.append(dfdetail)
             lret.append(d)
+            self.reset()
         return pd.concat(ldf), pd.DataFrame.from_dict(lret)
+
+    def compare_csvset(self, pred_dir, csvfname='./VAD_results_extended.csv'):
+
+        df = pd.read_csv(csvfname)
+        src = df.fileid.map(lambda x: './annotations/vad/%s.csv' % x)
+        dst = df.fileid.map(lambda x: '%s/%s.csv' % (pred_dir, x))
+        dfdetail, dret = self.compare_lfiles(src, dst)
+        self.reset()
+        return dfdetail, dret
